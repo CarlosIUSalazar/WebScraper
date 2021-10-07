13aad7e67eee3973f0975ecbf7a7181d8d15f9b8
Carlos Salazar <carlosiusalazar@gmail.com>
2021-10-07 18:41:35 +0900

    Adapted to scrape a Japanese word list from a site removing everything else

--- index.js
+++ index.js
@@ -4,26 +4,64 @@ const cheerio = require('cheerio')
 const express = require('express')
 
 const app = express()
-
-const url = 'http://www.theguardian.com/uk'
+const items = []
+const url = 'https://1000mostcommonwords.com/1000-most-common-japanese-words/'
 axios(url)
     .then((response) => {
         const html = response.data
         //console.log(html)
         const $ = cheerio.load(html)
-        const articles = []
+        
 
 
-        $('.fc-item__title', html).each(function() {
+        $('td', html).each(function() {
             const title = $(this).text()
-            const url = $(this).find('a').attr('href')
-            articles.push({
-                title,
-                url
-            })
-        })
-        console.log(articles)
-    }).catch(err => console.log(err))
+            if (title.toString().match(/[\u3000-\u303f\u3040-\u309f\u30a0-\u30ff\uff00-\uff9f\u4e00-\u9faf\u3400-\u4dbf]/)) {
+                items.push(title)
+            }
+        //console.log(items)
+        items.forEach(e => console.log(e))
+        }).catch(err => console.log(err))
+    })
 
 app.listen(PORT, () => console.log(`server running on port ${PORT}`))
 
+// function download() {
+// 'use strict'
+
+// const fs = require('fs');
+
+// let fileContent = items
+// fs.writeFileSync('message.txt', fileContent);
+// }
+
+// download()
+
+// const PORT = 8000
+// const axios = require('axios')
+// const cheerio = require('cheerio')
+// const express = require('express')
+
+// const app = express()
+
+// const url = 'http://www.theguardian.com/uk'
+// axios(url)
+//     .then((response) => {
+//         const html = response.data
+//         //console.log(html)
+//         const $ = cheerio.load(html)
+//         const articles = []
+
+
+//         $('.fc-item__title', html).each(function() {
+//             const title = $(this).text()
+//             const url = $(this).find('a').attr('href')
+//             articles.push({
+//                 title,
+//                 url
+//             })
+//         })
+//         console.log(articles)
+//     }).catch(err => console.log(err))
+
+// app.listen(PORT, () => console.log(`server running on port ${PORT}`))
